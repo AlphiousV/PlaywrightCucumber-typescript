17cea8ab852e3eea7cbc8cb2527b956e7a48c50e
AlphiousV <alphious28062000@gmail.com>
2026-06-22 21:41:46 +0530

    2 Scenario automated with report

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -7,13 +7,19 @@ dotenv.config(); // Loads the variables into process.env
 
 let Browser:Browser;
 
+const isCI = !!process.env.CI;
+
 BeforeAll(async function () {
        console.log("BeforeAll Started");
 
     Browser = await chromium.launch({
         headless:true,
         slowMo:5000,
-         args: ['--no-sandbox', '--disable-dev-shm-usage']
+         args: isCI ? [
+      '--no-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ] : [],
     });
 
       console.log("browser lauched Sucessfully");
@@ -23,7 +29,7 @@ Before(async function (this:customWorld) {
        console.log("Before Started");
      this.Context = await Browser.newContext();
     this.Page = await this.Context.newPage();
-    this.Page.setDefaultTimeout(15*1000)
+    this.Page.setDefaultTimeout(90*1000)
     this.Page.setDefaultNavigationTimeout(1000*30);
       console.log("Before executed Sucessfully");
 })
